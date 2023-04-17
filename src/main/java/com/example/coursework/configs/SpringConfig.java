09abe58765bb4517443d6c12927126d53a680878
Dss <sd;fjkwoiefjsldkfj@adjskjd.org>
2023-04-17 10:35:19 +0300

    Initial commit

--- src/main/java/com/example/coursework/configs/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.coursework.configs;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.coursework.utils")
-public class SpringConfig {
-}
--- src/main/java/com/example/coursework/enums/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.coursework.enums;
-
-public enum OrderStatus {
-
-    Принят, Оформлен, Ожидает, Получен
-}
